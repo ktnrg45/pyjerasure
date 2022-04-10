981601e633582197023368d24a87b396b5385ee5
ktnrg45 <ktnrg45@gmail.com>
2022-04-10 12:56:45 -0700

    Remove windows libs

--- pyjerasure/__init__.py
+++ pyjerasure/__init__.py
@@ -2,29 +2,6 @@
 import sys
 from typing import Iterable
 
-# pylint: disable=import-outside-toplevel
-def __load():
-    """Try to load dlls."""
-    from importlib import resources
-    import platform
-    import ctypes
-
-    try:
-        with resources.path("pyjerasure", ".libs") as lib_path:
-            lib_path /= f"{sys.platform}_{platform.machine()}".lower()
-        dll_files = [str(_file) for _file in lib_path.glob("*.dll")]
-        for dll in dll_files:
-            for lib_name in ("libJerasure", "libgf_complete"):
-                if lib_name in dll:
-                    ctypes.CDLL(dll)
-    except Exception:  # pylint: disable=broad-except
-        pass
-
-
-if sys.platform == "win32":
-    __load()
-
-# pylint: disable=wrong-import-position
 from .jerasure import Matrix, decode, encode
 
 
--- setup.py
+++ setup.py
@@ -95,11 +95,6 @@ CLASSIFIERS = [
 with open("README.md") as f:
     README = f.read()
 
-if sys.platform == "win32":
-    package_data = {"pyjerasure": [".libs/*/*.dll"]}
-else:
-    package_data = {}
-
 setup(
     name="pyjerasure",
     version=VERSION,
@@ -118,5 +113,4 @@ setup(
     test_suite="tests",
     ext_modules=build_extensions(),
     zip_safe=False,
-    package_data=package_data,
 )
