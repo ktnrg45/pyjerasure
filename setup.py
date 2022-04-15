886d16237e88a6fa562a6a248f17c251193d5268
ktnrg45 <ktnrg45dev@gmail.com>
2022-04-14 22:23:54 -0700

    Add Windows to cibuild

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 """Setup for Jerasure."""
 import sys
+import os
 import subprocess
 from pathlib import Path
 from setuptools import find_packages, setup, Extension
+
+
+ARG_INCLUDE = "--include="  # include dir
+ARG_LIB = "--lib="  # library dir
+PYJERASURE_INCLUDE = "PYJERASURE_INCLUDE"
+PYJERASURE_LIB = "PYJERASURE_LIB"
 
 
 def get_include_path():
@@ -37,14 +44,39 @@ def get_include_path():
 def build_extensions():
     """Return extensions."""
     library_dirs = []
-    if sys.platform == "win32":
-        include_path = "Include"
-        library_dirs = ["libs"]
-    else:
-        include_path = get_include_path()
+    include_dirs = []
+    # Check args first
+    args = list(sys.argv)
+    for arg in args:
+        if arg.startswith(ARG_INCLUDE):
+            include_dirs = [arg.split("=")[-1]]
+            sys.argv.remove(arg)
+            continue
+        if arg.startswith(ARG_LIB):
+            library_dirs = [arg.split("=")[-1]]
+            sys.argv.remove(arg)
+            continue
+
+    # Check env vars
+    if not include_dirs:
+        env_include = os.environ.get(PYJERASURE_INCLUDE)
+        if env_include:
+            include_dirs = [env_include]
+    if not library_dirs:
+        env_lib = os.environ.get(PYJERASURE_LIB)
+        if env_lib:
+            library_dirs = [env_lib]
+
+    if not include_dirs:
+        if sys.platform == "win32":
+            include_dirs = ["Include"]
+        else:
+            include_dirs = [get_include_path()]
+    if not library_dirs:
+        if sys.platform == "win32":
+            library_dirs = ["libs"]
 
     name = "pyjerasure.jerasure"
-    include_dirs = [include_path]
     libraries = ["Jerasure", "gf_complete"]
     try:
         # pylint: disable=import-outside-toplevel
