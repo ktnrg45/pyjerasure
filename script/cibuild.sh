411d481cfb4d989e9a6c41106db7b4e0b1338f5d
ktnrg45 <ktnrg45dev@gmail.com>
2022-05-06 00:17:28 -0700

    test macos

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 is_alpine=$(find "/etc/alpine-release")
-if [ -z $is_alpine ]; then
+[[ $OSTYPE == 'darwin'* ]] && is_macos=1
+echo $is_macos
+
+if [ -z $is_alpine ] && [ -z $is_macos ]; then
     apt update && apt install libjerasure-dev -y && exit 0
 fi
 
-apk add build-base autoconf automake libtool git
+if [ ! -z $is_macos ]; then
+    brew install autoconf automake libtool
+else
+    apk add build-base autoconf automake libtool git
+fi
 
 cd /
 git clone https://github.com/ktnrg45/jerasure.git --recurse-submodules
