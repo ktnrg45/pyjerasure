b588504fd8284065a050b2db9c14035ba8c74301
ktnrg45 <ktnrg45dev@gmail.com>
2022-05-06 00:39:04 -0700

    Add submodule

--- script/cibuild.sh
+++ script/cibuild.sh
@@ -2,7 +2,6 @@
 
 is_alpine=$(find "/etc/alpine-release")
 [[ $OSTYPE == 'darwin'* ]] && is_macos=1
-echo $is_macos
 
 if [ -z $is_alpine ] && [ -z $is_macos ]; then
     apt update && apt install libjerasure-dev -y && exit 0
@@ -14,8 +13,8 @@ else
     apk add build-base autoconf automake libtool git
 fi
 
-cd /
-git clone https://github.com/ktnrg45/jerasure.git --recurse-submodules
+# cd /
+# git clone https://github.com/ktnrg45/jerasure.git --recurse-submodules
 cd jerasure/gf-complete
 autoreconf -fvi
 ./configure
@@ -25,4 +24,4 @@ autoreconf -fvi
 ./configure
 make install -j4
 mv /usr/local/include/jerasure/* /usr/local/include
-cd /project
+cd ../
